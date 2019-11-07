da8f450af04944f8d4237c29ebd0490ebcc08701
Maycon Lima <55300838+limaycon@users.noreply.github.com>
2019-11-07 19:03:35 -0300

    Create styles.css

---
+++ codigo/styles.css
@@ -0,0 +1,20 @@
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  display: flex;
+  background: #FACC2E;
+}
+
+.cartoes {
+  width: 640px;
+  height: 640px;
+  margin: auto;
+  display: flex;
+  flex-wrap: wrap;
+  perspective: 1000px;
+}
